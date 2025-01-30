bfb6df67b052cce7f6afceaca4a6ac4b21330ec8
MartinNav <martinnavratilu@gmail.com>
2025-01-30 20:38:14 +0100

    added more tests

--- src/allocating.rs
+++ src/allocating.rs
@@ -67,14 +67,34 @@ mod tests_allocating {
     use super::*;
 
     #[test]
-    fn sanitize_string_allocating() {
+    fn sanitize_string_0() {
         let s = "<h1>hi!</h1>".to_string();
         assert_eq!("&lt;h1&rt;hi!&lt;/h1&rt;".to_string(), sanitize_string(s));
     }
 
     #[test]
-    fn remove_html_tags_allocating() {
+    fn remove_html_tags_0() {
         let s = "<h1>hi!</h1>".to_string();
+        assert_eq!("hi!".to_string(), remove_html_tags(s));
+    }
+    #[test]
+    fn remove_html_tags_1() {
+        let s = "<h1>>hi!</h1>".to_string();
+        assert_eq!(">hi!".to_string(), remove_html_tags(s));
+    }
+    #[test]
+    fn remove_html_tags_2() {
+        let s = "<h1<>>hi!</h1>".to_string();
+        assert_eq!("hi!".to_string(), remove_html_tags(s));
+    }
+    #[test]
+    fn remove_html_tags_3() {
+        let s = "<h1<p>>hi!</h1>".to_string();
+        assert_eq!("hi!".to_string(), remove_html_tags(s));
+    }
+    #[test]
+    fn remove_html_tags_4() {
+        let s = "<h1 onclick=\"alert(0)\">hi!</h1>".to_string();
         assert_eq!("hi!".to_string(), remove_html_tags(s));
     }
 }
