a70c7bf5598c86d4386d707abd9b25603273c933
MartinNav <martinnavratilu@gmail.com>
2025-01-30 16:09:37 +0100

    added test

--- src/allocating.rs
+++ src/allocating.rs
@@ -1,12 +1,22 @@
 pub fn sanitize_string<T:Into<String>>(input:T)->String{
     let a:String = input.into();
-    a.replace('<', "&lt").replace('>', "&rt")
+    a.replace('<', "&lt;").replace('>', "&rt;")
 }
 pub trait AllocatingSanitizer {
     fn sanitize(&mut self);
 }
 impl AllocatingSanitizer for String{
     fn sanitize(&mut self){
-        *self=self.replace('<', "&lt").replace('<', "&rt");
+        *self=self.replace('<', "&lt;").replace('<', "&rt;");
+    }
+}
+#[cfg(test)]
+mod tests_allocating {
+    use super::*;
+
+    #[test]
+    fn sanitize_string_allocating() {
+        let s = "<h1>hi!</h1>".to_string();
+        assert_eq!("&lt;h1&rt;hi!&lt;/h1&rt;".to_string(),sanitize_string(s));
     }
 }
