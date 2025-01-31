f2ee32b9d89da2f85311ce6fd1a95b78d24d18f7
MartinNav <martinnavratilu@gmail.com>
2025-01-31 12:51:57 +0100

    added prelude

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,17 @@
 pub mod allocating;
 pub mod nonallocating;
+/// # Examples
+/// ```
+/// use xssan::prelude::*;
+/// // string with html
+/// let a = "<h1>Title</h1><p> paragraph.</p>";
+/// // this function removes all html tags
+/// assert_eq!("Title paragraph.", remove_html_tags(a.clone()));
+/// // this function will convert the html tags to be displayed as html tags. 
+/// // But it will not be executed as html.
+/// assert_eq!("&lt;h1&rt;Title&lt;/h1&rt;&lt;p&rt; paragraph.&lt;/p&rt;", sanitize_string(a));
+///
+/// ```
+pub mod prelude{
+    pub use crate::allocating::*;
+}
