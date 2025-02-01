7ac740d57071408ca3bc8a69bfda5e3d6855758a
MartinNav <martinnavratilu@gmail.com>
2025-02-01 21:30:44 +0100

    made the function faster(O(2n)->O(n))+added docs

--- src/fast.rs
+++ src/fast.rs
@@ -1,5 +1,16 @@
-
+/// This function will remove all left an right brackets.
+/// Therefore the html tags will no longer be interpreted as html
+/// ## Performance
+/// This function is one of the fastest. But because it requires to copy the entire string it is
+/// not adviced to be used on large strings.(Large string is above 100k characters.) 
+/// This function is O(n).
+/// # Examples
+/// ```
+/// use xssan::fast::*;
+/// assert_eq!("h1hello/h1", remove_brackets("<h1>hello</h1>"));
+///
+/// ```
 pub fn remove_brackets<T:Into<String>>(input:T)->String{
     let input:String = input.into();
-    input.replace('<', "").replace('>', "")
+    input.chars().filter(|x| !(*x=='<'||*x=='>')).collect::<String>()
 }
