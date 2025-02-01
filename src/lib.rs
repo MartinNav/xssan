f014ffba4359bdeba89c426bf9043c9519a9f9df
MartinNav <martinnavratilu@gmail.com>
2025-02-01 21:34:43 +0100

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -11,12 +11,12 @@ pub mod fast;
 /// let a = "<h1>Title</h1><p> paragraph.</p>";
 /// // this function removes all html tags
 /// assert_eq!("Title paragraph.", remove_html_tags(a.clone()));
-/// // this function will convert the html tags to be displayed as html tags. 
+/// // this function will convert the html tags to be displayed as html tags.
 /// // But it will not be executed as html.
 /// assert_eq!("&lt;h1&rt;Title&lt;/h1&rt;&lt;p&rt; paragraph.&lt;/p&rt;", sanitize_string(a));
 ///
 /// ```
-pub mod prelude{
+pub mod prelude {
     pub use crate::allocating::*;
     pub use crate::fast::*;
 }
